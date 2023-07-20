9d2e2f4625ba38bc8e83b057a652396d435fe1f3
jipstavenuiter <jipstavenuiter@gmail.com>
2023-07-20 16:50:22 +0800

    fix: update round address

--- packages/api/src/constants.ts
+++ packages/api/src/constants.ts
@@ -1,10 +1,10 @@
 export const backupRounds: Record<string, string> = {
-  "0x0524d9f611b3785f4675c90aa3161ab0e54c4a39":
+  "0xfc68882a250f5c444f737b8e4ffbfa6ca769efcd":
     "0xa2ae8421776035c398c22e143290697da09d19d7"
 };
 
 export const ignoredAddresses: Record<string, string[]> = {
-  "0x0524d9f611b3785f4675c90aa3161ab0e54c4a39": [
+  "0xfc68882a250f5c444f737b8e4ffbfa6ca769efcd": [
     "0x4bfd2181be8fa2f6702dee41a46baabeb5d3dd3d",
     "0x16e41af5e034113802d06064fbeb020cc3d47b19"
   ]
