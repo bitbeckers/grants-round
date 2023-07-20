223f63aac234a3b93ff812ea5fa83eb45fe160c2
jipstavenuiter <jipstavenuiter@gmail.com>
2023-07-20 15:08:21 +0800

    fix: apply hotfix for round summary

--- packages/api/src/constants.ts
+++ packages/api/src/constants.ts
@@ -1,3 +1,4 @@
 export const backupRounds: Record<string, string> = {
-  "0x000000": "0xa2ae8421776035c398c22e143290697da09d19d7"
+  "0x0524d9f611b3785f4675c90aa3161ab0e54c4a39":
+    "0xa2ae8421776035c398c22e143290697da09d19d7"
 };
